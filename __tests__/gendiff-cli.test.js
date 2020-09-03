8524eda95076f79291a507c5399862d03f234650
marchkatte <marchkatte@gmail.com>
2020-09-03 21:18:46 +0300

    Fixed CC

--- __tests__/gendiff-cli.test.js
+++ __tests__/gendiff-cli.test.js
@@ -1,25 +1,11 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
-import genDiff, { getDataOfFile } from '../src/gendiff-cli.js';
+import genDiff from '../src/gendiff-cli.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
-
-const object1 = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-
-const objectini = {
-  host: 'hexlet.io',
-  timeout: '50',
-  proxy: '123.234.53.22',
-  follow: false,
-};
 
 const expected = `{
   - follow: false
@@ -29,12 +15,6 @@ const expected = `{
   + timeout: 20
   + verbose: true
 }`;
-
-test('getDataOfFile', () => {
-  expect(getDataOfFile(getFixturePath('before.json'))).toEqual(object1);
-  expect(getDataOfFile(getFixturePath('before.yml'))).toEqual(object1);
-  expect(getDataOfFile(getFixturePath('before.ini'))).toEqual(objectini);
-});
 
 test('genDiff', () => {
   expect(genDiff(getFixturePath('before.json'), getFixturePath('after.json'))).toEqual(expected);
