e110c5cd21be9eed398ed03f6c11a4f08226a827
marchkatte <marchkatte@gmail.com>
2020-09-15 23:32:06 +0300

    Fixed formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,7 +1,14 @@
+import _ from 'lodash';
 import makeStylish from './stylish.js';
 import makePlain from './plain.js';
 
 const formatters = { stylish: makeStylish, plain: makePlain, json: JSON.stringify };
-const makeFormat = (data, format) => formatters[format](data);
+
+const makeFormat = (data, format) => {
+  if (!_.has(formatters, format)) {
+    throw new Error(`${format} is unknown format.`);
+  }
+  return formatters[format](data);
+};
 
 export default makeFormat;
