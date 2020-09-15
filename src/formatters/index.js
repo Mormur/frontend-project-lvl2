54f44a7598ae7ce66e9bd44dc11745a0f6392680
marchkatte <marchkatte@gmail.com>
2020-09-15 23:21:59 +0300

    Renew formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,20 +1,7 @@
 import makeStylish from './stylish.js';
 import makePlain from './plain.js';
 
-// const formatters = { stylish: makeStylish, plain: makePlain };
-// const makeFormat = (data, format) => formatters[format](data);
-
-const makeFormat = (data, format) => {
-  switch (format) {
-    case 'plain':
-      return makePlain(data);
-    case 'stylish':
-      return makeStylish(data);
-    case 'json':
-      return JSON.stringify(data);
-    default:
-      throw new Error(`${format} is unknown format!`);
-  }
-};
+const formatters = { stylish: makeStylish, plain: makePlain, json: JSON.stringify };
+const makeFormat = (data, format) => formatters[format](data);
 
 export default makeFormat;
